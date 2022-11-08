cb4fc2e3c26344e2389efbf615a8d4bdf788fb68
Minji Chun(천민지) <sloth@bluehole.net>
2022-11-08 18:32:47 +0900

    Use relative path for js and css

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -5,6 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '',
   plugins: [vue()],
   resolve: {
     alias: {
@@ -12,6 +13,6 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: "../dist"
+    outDir: "../docs"
   }
 });
